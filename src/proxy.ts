d66594ec040ba513c07c3ae73443f33c35337245
bukalla <oumerelfaruke@gmail.com>
2026-06-25 23:49:04 +0300

    updated the proxy name

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest): NextResponse {
+export function proxy(request: NextRequest): NextResponse {
   const { pathname } = request.nextUrl;
   
   // Get cookies
